d5548ffaef3100de967e89cbd9ae7a3274cc9584
hjelleyman <38963036+hjelleyman@users.noreply.github.com>
2019-11-29 10:49:18 +1300

    added environment file and modules folder

---
+++ modules/__init__.py
@@ -0,0 +1,11 @@
+"""modules
+
+This contains all the helper scripts for each notebook.
+
+Scripts
+------------
+
+    lorentz
+    -------
+        The code for the lorentz notebook. This notebook focuses on Special Relativity
+"""
