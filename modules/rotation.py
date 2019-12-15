5195e53b24df3bbfb7a5b0bac8a2cda6c515aea9
Hamish Jelleyman <hamish.jelleyman@gmail.com>
2019-12-16 08:45:17 +1300

    updates

--- modules/rotation.py
+++ modules/rotation.py
@@ -9,16 +9,14 @@ This script contains helper functions for the rotation notebook.
 import numpy as np
 import matplotlib.pyplot as plt
 from ipywidgets import interactive, FloatSlider
-# plt.style.use('modules/matplotlibrc')
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
 
-time=np.linspace(-6,20,100)
-space=np.linspace(-20,20,100)
-line1=np.linspace(-20,20,100)
-line2=np.linspace(20,-20,100)
-line3=np.zeros(11)
-line4=np.linspace(0,10,11)
-line5=np.zeros(len(line3))
-line6=np.zeros(len(line3))
+
+plt.style.use('modules/matplotlibrc')
+
 from modules import lorentz as lz
 
 
@@ -50,7 +48,7 @@ def plot_euclidian_vector():
         plt.xlim([-1.1,1.1])
         plt.ylim([-1.1,1.1])
         
-        plt.legend(loc='center left', bbox_to_anchor= (1.0, 0.5), ncol=1, borderaxespad=0.5, frameon=False)
+        plt.legend(loc='center left', bbox_to_anchor= (1, 0.5), ncol=1, borderaxespad=0.5, frameon=False)
         plt.plot
         
     interactive_plot = interactive(f, theta=FloatSlider(min=0, max=2*np.pi, step=1e-4, continuous_update=False, description="$\\theta$", value=np.pi/4))
@@ -58,6 +56,89 @@ def plot_euclidian_vector():
 #     output.layout.height = '650px'
     return interactive_plot
 
+
+def animate_plot_1():
+    fig, ax = plt.subplots(figsize=(10,10))
+    plt.plot([-2,2],[0,0],'k', alpha = 0.1)
+    plt.plot([0,0],[-2,2],'k', alpha = 0.1)
+    line, = ax.plot([],[],'r', label = '$[x,y]=[sin(\\theta), cos(\\theta)]$')
+    xangle, = ax.plot([],[],'r')
+    text = plt.text(0.14*np.cos(np.pi/4), 0.14*np.sin(np.pi/4),'')
+    
+    # x length
+    xlength, = plt.plot([], [],'g')
+    xtext = plt.text(0, 0,'x')
+    # y length
+    ylength, = plt.plot([], [],'b')
+    ytext = plt.text(0, 0,'y')
+
+    # axis stuff
+    plt.axis('off')
+    plt.xlim([-1.1,1.1])
+    plt.ylim([-1.1,1.1])
+    
+    def init():
+        line.set_data([], [])
+        xangle.set_data([],[])
+        text.set_text('')
+        xlength.set_data([],[])
+        xlength.set_label('')
+        xtext.set_position([0,0])
+        xtext.set_text('')
+        ylength.set_data([],[])
+        ylength.set_label('')
+        ytext.set_position([0,0])
+        ytext.set_text('')
+        return [line,text,xangle,xlength,xtext,ylength,ytext]
+    
+    def animate(theta):
+        theta = theta *np.pi/50
+        thetavec = np.linspace(0,theta)
+        x = np.cos(theta)
+        y = np.sin(theta)
+#         plt.legend(loc='center left', bbox_to_anchor= (1.0, 0.5), ncol=1, borderaxespad=0.5, frameon=False)
+
+        line.set_data([0,x], [0,y])
+        xangle.set_data(0.1*np.cos(thetavec), 0.1*np.sin(thetavec))
+        text.set_text('$\\theta = $'+'{:.2f} radians'.format(theta))
+        xlength.set_data([0,x], [y,y])
+        xlength.set_label('x = {:.02f}'.format(x))
+        xtext.set_position((x/2, y+0.05))
+        xtext.set_text('x = {:.2f}'.format(x))
+        ylength.set_data([x,x], [0,y])
+        ylength.set_label('y = {:.02f}'.format(y))
+        ytext.set_position((x+0.05, y/2))
+        ytext.set_text('y = {:.2f}'.format(y))
+        return [line,text,xangle,xlength,xtext,ylength,ytext]
+
+    
+#     plt.plot([0,x], [0,y],'r',label = '$[x,y]=[sin(\\theta), cos(\\theta)]$')
+#     plt.plot(0.1*np.cos(thetavec), 0.1*np.sin(thetavec))
+#     plt.text(0.13*np.cos(np.pi/4), 0.1*np.sin(np.pi/4),'$\\theta = $'+'{:.2f} radians'.format(theta))
+
+#     # x length
+#     plt.plot([0,x], [y,y],'g',label = 'x = {:.02f}'.format(x))
+#     plt.text(x/2, y+0.05,'x')
+#     # y length
+#     plt.plot([x,x], [0,y],'b',label = 'y = {:.02f}'.format(y))
+#     plt.text(x+0.05, y/2,'y')
+
+#     plt.plot([],[],label = '$\\sqrt{x^2+y^2}=$'+'{:.2f}'.format(np.sqrt(x**2+y**2)))
+
+#     # axis stuff
+#     plt.axis('off')
+#     plt.xlim([-1.1,1.1])
+#     plt.ylim([-1.1,1.1])
+
+#     plt.legend(loc='center left', bbox_to_anchor= (1.0, 0.5), ncol=1, borderaxespad=0.5, frameon=False)
+    
+    
+    
+    ani = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=100, interval=100, blit=True)
+    return HTML(ani.to_jshtml())
+    
+    
 
 def plot_2_euclidian_vectors():
     
