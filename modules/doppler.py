19a6d9da85eb682d9b0afee675902bb0c5da29e4
Hamish Jelleyman <hamish.jelleyman@gmail.com>
2020-01-24 13:41:31 +1300

    finished doppler
    Ended up with the expected elongated ellipse result

--- modules/doppler.py
+++ modules/doppler.py
@@ -141,27 +141,10 @@ def animate_transverse_moving_source(N=200):
 
 #-------------------------------------------------- WIP --------------------------------------------------
 
-def plot_relativistic_observer(v=0, c = 3e8, N=200, v_wave = 3e8):
-    
+def full_doppler(v=0, c = 3e8, N=200, v_wave = 3e8, freq = 20, relativistic=True,classical=False):
     
     theta = np.linspace(0,2*np.pi,100)
     time = np.arange(N)
-
-    def run(t, tstart, circles):
-        if t % (N//10) == 0:
-            tstart += [t]
-            circles += plt.plot([],[],'black')
-            
-        for i in range(len(circles)):
-            
-            R = v_wave*(t - tstart[i])
-            
-            betax = v*np.cos(theta)/c
-            xvec = (R * np.cos(theta)) * np.sqrt((1+betax)/(1-betax))
-            
-            yvec = R*np.sin(theta)
-            
-            circles[i].set_data(xvec,yvec)
 
     fig, ax = plt.subplots(figsize = (10,10))
     # axis stuff
@@ -171,8 +154,60 @@ def plot_relativistic_observer(v=0, c = 3e8, N=200, v_wave = 3e8):
     plt.ylim([-v_wave*N*1.5,v_wave*N*1.5])
     
     source, = plt.plot([0],[0], 'o', color = 'black', markersize = 10)
-    circles = []
+    x0 = np.linspace(-v*N*1/2,v*N*1/2,N)
+    y0 = np.zeros(N)
+    rel_circles = []
+    clas_circles = []
     tstart = []
+    xstart = []
     
-    ani = animation.FuncAnimation(fig, run, frames = time, blit=False, interval=10000/N, repeat=True, fargs = [tstart, circles])
+    
+    def run(t, tstart, xstart, rel_circles, clas_circles):
+        source.set_data(x0[t],y0[t])
+        if t % freq == 0:
+            tstart += [t]
+            xstart += [x0[t]]
+            if relativistic:
+                rel_circles += plt.plot([],[],'black')
+            if classical:
+                clas_circles += plt.plot([],[],'--',color = 'red')
+                
+                
+        if relativistic:    
+            for i in range(len(rel_circles)):
+                circle = rel_circles[i]
+                
+                # Beta will vary depending on the angle between the motion of the wave leaving the source and the motion of the source.
+                beta = v/c*np.cos(theta)
+
+                t_source = (t - tstart[i])  # For the source, each wave leaves at an even time period.
+                d_source = v_wave * t_source # Then the wave propergates outwards at a constant velocity (circle).
+                
+                # We can use a lorentz transform to work out the time this takes for a stationary observer.
+                t_observer = t_source / np.sqrt(1-beta**2) 
+                # Because the time dilation is different for each angle we need to scale our results so the distance a wavefront travels is the distance it does in a set time period.
+                d_observer = t_observer * d_source / t_source 
+                
+                # Parameterising the expansion of the wave and moving the source of each wavefront.
+                x = d_observer * np.cos(theta) + xstart[i] 
+                y = d_observer * np.sin(theta)
+
+                circle.set_data(x,y)
+        
+        if classical:
+            for i in range(len(clas_circles)):
+                circle = clas_circles[i]
+
+                t_observed = (t - tstart[i])
+                d = v_wave * t_observed
+
+                x = d * np.cos(theta) + xstart[i]
+                y = d * np.sin(theta)
+
+                circle.set_data(x,y)
+            
+            
+    
+    ani = animation.FuncAnimation(fig, run, frames = time, blit=False, interval=10000/N, repeat=True, fargs = [tstart, xstart, rel_circles, clas_circles])
     return HTML(ani.to_jshtml())
+    
