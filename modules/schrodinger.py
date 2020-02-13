4df48faf3847b49b19f6f103791f729d9409c830
Hamish Jelleyman <hamish.jelleyman@gmail.com>
2020-02-13 14:06:55 +1300

    updates

--- modules/schrodinger.py
+++ modules/schrodinger.py
@@ -1,55 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-from scipy.integrate import odeint
-
-start_position=-20.0
-end_position=20   #set this to be the correct end point.
-space_steps=2000
-step=(end_position-start_position)/(space_steps-1)
-space=np.linspace(start_position,end_position,space_steps)
-
-def plot_potential(psi,psiprime):
-    """Plots a potential function and its derivative.
+from IPython.display import display_html
+def display_side_by_side(*args):
+    html_str=''
+    for df in args:
+        html_str+=df.to_html()
+    display_html(html_str.replace('table','table style="display:inline"'),raw=True)
     
-    Parameters
-    ----------
-    psi : list-like, length 2000.
-        The potential.
-    psiprime : list-like, length 2000.
-        The first derivative of the potential.
-
-    Returns
-    -------
-    no value
-    """
-    # Create the matplotlib figure and plot functions
-    plt.figure()
-    plt.plot(space,psi,linewidth=3,label='wavefunction')
-    plt.plot(space,psiprime,linewidth=3,label='derivative')
-    plt.legend()
-    plt.show()
+def splitdf(df, n = 3):
+    split = [df.iloc[i*(len(df)//n):(i+1)*(len(df)//n)] for i in range(n-1)] + [df.iloc[(n-1)*(len(df)//n):]]
+    display_side_by_side(*split)
     
-def plot_potential_with_scaled(psi,psiprime, scaled_potential):
-    """Plots a potential function and its derivative, as well as a scaled potential function.
-    
-    Parameters
-    ----------
-    psi : list-like, length 2000.
-        The wavefunction.
-    psiprime : list-like, length 2000.
-        The first derivative of the wavefunction.
-    scaled_potential : list-like, length 2000
-        A scaled potential.
-
-    Returns
-    -------
-    no value
-    """
-    # Create the matplotlib figure and plot functions
-    plt.figure()
-    plt.plot(space,psi,linewidth=3,label='wavefunction')
-    plt.plot(space,psiprime,linewidth=3,label='derivative')
-    plt.plot(space,scaled_potential/8+0.5,linewidth=1,label='scaled potential') #show the potential well as well
-    plt.legend()
-    plt.show()
